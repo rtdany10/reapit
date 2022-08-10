2991a0612b4ba750d272a3afd3222f4aae430eee
Dany Robert <rtdany10@gmail.com>
2022-08-10 10:03:38 +0530

    patch: update refurb cost

---
+++ reapit/patches/update_refurbish_cost.py
@@ -0,0 +1,16 @@
+import frappe
+
+def execute():
+	entries_to_fix = frappe.db.get_all("Stock Entry", filters={
+		"docstatus": 1,
+		"purpose": "Repack",
+		"refurbishment_cost": ["<", 0.0]
+	}, pluck="name")
+	for se in entries_to_fix:
+		doc = frappe.get_doc("Stock Entry", se)
+		rf_cost = 0.0
+		for row in doc.items:
+			if row.item_code != '2001' and not row.t_warehouse:
+				rf_cost += row.basic_amount
+		doc.db_set("refurbishment_cost", rf_cost)
+				
