a1f3b1950e688d6ec97c244ce527905d28183655
Dany Robert <rtdany10@gmail.com>
2022-09-15 13:16:20 +0530

    chore: generate pdf only if repack entry

--- reapit/tasks.py
+++ reapit/tasks.py
@@ -353,16 +353,17 @@ def make_stock_in_entry(source_name, item_dict, target_doc=None):
 
 
 def attach_pdf(doc, method=None):
-	pdf = frappe.attach_print(
-		doc.doctype,
-		doc.name,
-		doc=doc
-	)
-	frappe.get_doc({
-		"doctype": "File",
-		"file_name": pdf["fname"],
-		"is_private": 0,
-		"content": pdf["fcontent"],
-		"attached_to_doctype": doc.doctype,
-		"attached_to_name": doc.name
-	}).save()
+	if doc.purpose == "Repack":
+		pdf = frappe.attach_print(
+			doc.doctype,
+			doc.name,
+			doc=doc
+		)
+		frappe.get_doc({
+			"doctype": "File",
+			"file_name": pdf["fname"],
+			"is_private": 0,
+			"content": pdf["fcontent"],
+			"attached_to_doctype": doc.doctype,
+			"attached_to_name": doc.name
+		}).save()
