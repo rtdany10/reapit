cd74a62e0da1739e4b120203ccc22edb77943e1b
niyazrazak <niyasibnurazak@gmail.com>
2023-10-07 12:44:12 +0000

    fix: set as cancelled

--- reapit/patches/remove_duplicate_entries.py
+++ reapit/patches/remove_duplicate_entries.py
@@ -9,7 +9,7 @@ Not sure how this happened, but it was done manually on the dashboard. Maybe con
 def execute():
     entries = ["MAT-STE-2022-338492", "MAT-STE-2023-94586"]
     for row in entries:
-        frappe.db.sql(""" delete from `tabGL Entry` where voucher_no = %s """, row)
-        frappe.db.sql(""" delete from `tabStock Ledger Entry` where voucher_no = %s """, row)
-        frappe.db.sql(""" delete from `tabStock Entry` where name = %s """, row)
-        frappe.db.sql(""" delete from `tabStock Entry Detail` where parent = %s """, row)
+        frappe.db.sql(""" UPDATE `tabGL Entry` SET is_cancelled = 1 where voucher_no = %s """, row)
+        frappe.db.sql(""" UPDATE `tabStock Ledger Entry` SET is_cancelled = 1 where voucher_no = %s """, row)
+        frappe.db.sql(""" UPDATE `tabStock Entry` SET docstatus = 2 where name = %s """, row)
+        frappe.db.sql(""" UPDATE `tabStock Entry Detail` SET docstatus = 2 where parent = %s """, row)
